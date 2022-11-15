21bb48e1c1e6249d4904912728b9a88093d05e81
zziyeon <jiyunsara99@naver.com>
2022-11-16 02:45:01 +0900

    수정중

--- blog.js
+++ blog.js
@@ -1,15 +1,66 @@
-const butt = document.querySelector('.btn');
+// nav
+const nav_home = document.querySelector('.nav_home');
+const nav_aboutMe = document.querySelector('.nav_aboutMe');
+const nav_skills = document.querySelector('.nav_skills');
+const nav_portfolio = document.querySelector('.nav_portfolio');
+const nav_contact = document.querySelector('.nav_contact');
 
-butt.addEventListener('click',e=>{console.log('클릭됨');});
+//nav 높이
+const nav_height = document.querySelector('nav').offsetHeight;
 
-function nextBtn(){
-    console.log('클릭됨');
-    const b_block = document.querySelector('.two');
+// 페이지 위치
+const scroll_aboutMe = document.getElementById('aboutMe').offsetTop;
+const scroll_skills = document.getElementById('skills').offsetTop;
+const scroll_portfolio = document.getElementById('portfolio').offsetTop;
+const scroll_contact = document.getElementById('contact').offsetTop;
 
-    if(b_block.style.display != 'none'){
-        b_block.style.display = 'none';
-    }else {
-        b_block.style.display ='block';
+nav_home.addEventListener('click', e=>{
+    window.scrollTo({top:0, behavior:'smooth'});
+});
+
+nav_aboutMe.addEventListener('click', e=>{
+    window.scrollTo({top:scroll_aboutMe-nav_height, behavior:'smooth'});
+});
+
+nav_skills.addEventListener('click', e=>{
+    window.scrollTo({top:scroll_skills-nav_height, behavior:'smooth'});
+});
+
+nav_portfolio.addEventListener('click', e=>{
+    window.scrollTo({top:scroll_portfolio-nav_height, behavior:'smooth'});
+});
+
+nav_contact.addEventListener('click', e=>{
+    window.scrollTo({top:scroll_contact-nav_height, behavior:'smooth'});
+});
+
+
+
+// aboutMe_right 버튼
+const prev = document.querySelector('.prevBtn');
+const next = document.querySelector('.nextBtn');
+
+function prevBtn(e){
+    console.log('클릭1');
+    const one = document.querySelector('.one');
+    const two = document.querySelector('.two');
+    
+    if(one.style.display == 'none'){
+        one.style.display = 'block';
+        two.style.display = 'none';
     }
 }
 
+function nextBtn(e){
+    console.log('클릭2');
+    const one = document.querySelector('.one');
+    const two = document.querySelector('.two');
+    
+    if(two.style.display == 'none'){
+        two.style.display = 'block';
+        one.style.display = 'none';
+    }
+}
+
+prev.addEventListener('click',prevBtn);
+next.addEventListener('click',nextBtn);
